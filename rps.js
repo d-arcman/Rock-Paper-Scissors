474a3804586b064249cabd39b2b097bbe63ce627
Sam Darcy <sam_darcy92@hotmail.com>
2022-05-22 15:27:52 +1000

    Make playRound function that gives result text based on player/computer selection

--- rps.js
+++ rps.js
@@ -9,4 +9,29 @@ function computerPlay () {
     let myAnswer = answers[Math.floor(Math.random()* answers.length)]
     return(myAnswer);
 }
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return("You win! Rock beats Scissors");
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return("You lose! Paper beats Rock");
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return("You win! Scissors beats Paper");
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return("You lose! Rock beats Scissors");
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return("You Win! Paper beats Rock");
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return("You lose! Scissors beats Paper");
+    } else if (playerSelection == 'rock' && computerSelection == 'rock') {
+        return("It's a Tie!");
+    } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
+        return("It's a Tie!");
+    } else if (playerSelection == 'paper' && computerSelection == 'paper') {
+            return("It's a Tie!");
+    } else return("No result");
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay ();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
